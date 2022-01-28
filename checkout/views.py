185177567fc6bf1a66e25b8973cc857395b20ffa
Harbs1999 <jakeleeharbour@hotmail.com>
2022-01-28 16:27:23 +0000

    Added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51KEGsHKU8JxANin5v2WhyRXbGPwz2A1ixi5DQFCTx7Ozt1sXvLD3UTyJzRGLJZ3vEljku6CirnsrEYeeqlSUiLHx00T80ZwKbi',
+        'clent_secret': 'test client secret',
     }
 
     return render(request, template, context)
