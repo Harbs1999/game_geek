db6b1bf5267f768b44a4fdd6fb6110b59d37a0f7
Harbs1999 <jakeleeharbour@hotmail.com>
2022-01-29 11:08:27 +0000

    Removed sevret key and set debug

--- game_geek/settings.py
+++ game_geek/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*pvz-e)^tm8mvkw84=$7_&4qq7zon5#hc0e3=ratuhvf0)1d@('
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['harbs1999-game-geek.herokuapp.com', 'localhost']
 
